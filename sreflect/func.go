dcb3d0b6bed813cab5db726db64a5015d5ceeec0
yyliziqiu <yyliziqiu@163.com>
2025-03-25 18:43:17 +0800

    代码优化

--- sreflect/func.go
+++ sreflect/func.go
@@ -6,7 +6,18 @@ import (
 	"strings"
 )
 
+var FuncNamePrefixes []string
+
 func FuncName(f any) string {
-	name := runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name()
-	return strings.Split(name, "-")[0]
+	func0 := runtime.FuncForPC(reflect.ValueOf(f).Pointer())
+
+	name := strings.Split(func0.Name(), "-")[0]
+
+	for _, prefix := range FuncNamePrefixes {
+		if strings.HasPrefix(name, prefix) {
+			return strings.TrimPrefix(name, prefix)
+		}
+	}
+
+	return name
 }
