ead6e8fd2dfb76ee8cee7b3312b5f43aea29cc86
yyliziqiu <yyliziqiu@163.com>
2025-06-13 15:02:51 +0800

    代码优化

--- ssnap/snap.go
+++ ssnap/snap.go
@@ -1,5 +1,9 @@
 package ssnap
 
+import (
+	"path/filepath"
+)
+
 type Snap struct {
 	path string
 	data any
@@ -23,4 +27,10 @@ func (s *Snap) Save() error {
 
 func (s *Snap) Load() error {
 	return Load(s.path, s.data)
+}
+
+func (s *Snap) SaveDuplicate(n int) error {
+	name := filepath.Base(s.path)
+	path := filepath.Join(filepath.Dir(s.path), "duplicates", name)
+	return Save(path, s.data)
 }
