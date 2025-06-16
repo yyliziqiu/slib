8a2ea75927da2c97293ba2f0254d08cdb74e12ae
yyliziqiu <yyliziqiu@163.com>
2025-06-16 15:26:56 +0800

    ssnap 测试增加快照副本功能

--- ssnap/snap_test.go
+++ ssnap/snap_test.go
@@ -1,1 +1,17 @@
 package ssnap
+
+import (
+	"testing"
+	"time"
+)
+
+func TestDuplicate(t *testing.T) {
+	data := map[string]string{
+		"hello": "world",
+	}
+
+	err := Duplicate("/private/ws/self/slib/data/test.json", data, 10*time.Second)
+	if err != nil {
+		t.Error(err)
+	}
+}
