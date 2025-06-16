525b6b4368ff60f4827a7914ed2286306d8826e2
yyliziqiu <yyliziqiu@163.com>
2025-06-16 16:36:32 +0800

    ssnap 增加快照副本功能

---
+++ ssnap/watcher_test.go
@@ -0,0 +1,41 @@
+package ssnap
+
+import (
+	"testing"
+	"time"
+
+	"golang.org/x/net/context"
+)
+
+func TestWatchers(t *testing.T) {
+	data1 := map[string]string{
+		"hello": "world1",
+	}
+	data2 := map[string]string{
+		"hello": "world2",
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := Watchers(ctx, []Setting{
+		{
+			Path: "/private/ws/self/slib/data/data1.json",
+			Data: &data1,
+		},
+		{
+			Path: "/private/ws/self/slib/data/data2.json",
+			Data: &data2,
+			Poll: 5 * time.Second,
+		},
+	}...)
+	if err != nil {
+		t.Error(err)
+	}
+
+	time.Sleep(30 * time.Second)
+
+	cancel()
+
+	time.Sleep(time.Second)
+}
