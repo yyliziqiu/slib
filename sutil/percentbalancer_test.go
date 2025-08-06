1e8e62fa1206c7ad9c6d7711e0a3ec3e5acb2b8a
yyliziqiu <yyliziqiu@163.com>
2025-08-06 10:07:31 +0800

    add sutil

---
+++ sutil/percentbalancer_test.go
@@ -0,0 +1,20 @@
+package sutil
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPercentBalancer(t *testing.T) {
+	r := NewPercentBalancer(34)
+
+	a, c := 0, 5235252
+	for i := 0; i < c; i++ {
+		ok := r.Take()
+		if ok {
+			a++
+		}
+	}
+
+	fmt.Printf("a = %d, c = %f\n", a, float64(a)/float64(c))
+}
---
+++ sutil/weightround.go
@@ -0,0 +1,4 @@
+package sutil
+
+type WeightRound struct {
+}
